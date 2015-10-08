64c2f5095cede425392e0be53a27fe430e63968f
seongho <zudain@naver.com>
2015-10-08 10:28:59 +0000

    kmooc 소문자

--- lms/static/js/discovery/search_facets_view.js
+++ lms/static/js/discovery/search_facets_view.js
@@ -101,7 +101,7 @@ define([
             }
             else{
                 //return term ? term : name;
-                var arrWords = [ ['khuk','koreaunivk', 'pnuk', 'snuk', 'skkuk', 'ysuk', 'ewhak', 'postechk', 'kaistk', 'hyuk', 'KMOOC'],
+                var arrWords = [ ['khuk','koreaunivk', 'pnuk', 'snuk', 'skkuk', 'ysuk', 'ewhak', 'postechk', 'kaistk', 'hyuk', 'kmooc'],
                 ['경희대학교','고려대학교','부산대학교','서울대학교','성균관대학교','연세대학교','이화여자대학교','포항공과대학교','한국과학기술원','한양대학교', 'K-MOOC'] ];
                 for (var i=0; i < arrWords[0].length; i++) {
                     if (term == arrWords[0][i]) {
