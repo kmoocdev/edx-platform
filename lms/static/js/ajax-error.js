0b738c787ed1dea9191c4ed03b42333c32d538a7
seongho <zudain@naver.com>
2015-10-27 09:24:43 +0000

    메세지 영문에서 국문으로 변경

--- lms/static/js/ajax-error.js
+++ lms/static/js/ajax-error.js
@@ -1,10 +1,10 @@
 $(document).ajaxError(function (event, jXHR) {
     if (jXHR.status === 403 && jXHR.responseText === 'Unauthenticated') {
         var message = gettext(
-            'You have been logged out of your edX account. '+
-            'Click Okay to log in again now. '+
-            'Click Cancel to stay on this page '+
-            '(you must log in again to save your work).'
+            '현재 K-MOOC에서 로그아웃되신 상태입니다. '+
+            '확인버튼을 클릭하시면 로그인페이지로 이동합니다. '+
+            '취소버튼을 클릭하시면 현재 페이지에 머무릅니다. '+
+            '(작업한 내역을 정확히 저장하시려면 반드시 로그인하세요)'
         );
 
         if (window.confirm(message)) {
