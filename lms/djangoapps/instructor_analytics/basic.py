769c027684adb32e31279f104de530239ed94448
shevious <shevious@yahoo.com>
2015-11-23 08:37:06 +0000

    프로필 다운로드에 국가, city 추가

--- lms/djangoapps/instructor_analytics/basic.py
+++ lms/djangoapps/instructor_analytics/basic.py
@@ -20,7 +20,7 @@ from student.models import CourseEnrollmentAllowed
 
 STUDENT_FEATURES = ('id', 'username', 'first_name', 'last_name', 'is_staff', 'email')
 PROFILE_FEATURES = ('name', 'language', 'location', 'year_of_birth', 'gender',
-                    'level_of_education', 'mailing_address', 'goals', 'meta')
+                    'level_of_education', 'mailing_address', 'goals', 'meta', 'country', 'city')
 ORDER_ITEM_FEATURES = ('list_price', 'unit_cost', 'status')
 ORDER_FEATURES = ('purchase_time',)
 
