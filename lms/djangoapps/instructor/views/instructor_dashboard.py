14256b275f168c1eff7a55d515bd059998f8d880
seongho <zudain@naver.com>
2015-12-01 06:27:19 +0000

    mongodb, mysql localhost

--- lms/djangoapps/instructor/views/instructor_dashboard.py
+++ lms/djangoapps/instructor/views/instructor_dashboard.py
@@ -607,151 +607,151 @@ def return_course(course_id):
     return course
 
 
-# def get_assessment_info(course):
-#     # TODO : ip
-#     # client = MongoClient('mongodb://192.168.1.112:27010/')
-#     client = MongoClient()
-#     db = client.edxapp
-#     cursor = db.modulestore.active_versions.find({'search_targets.wiki_slug':course.wiki_slug})
-#     for document in cursor:
-#         published_branch = document.get('versions').get('published-branch')
-#     cursor.close()
-#
-#     cursor = db.modulestore.structures.find({'_id':published_branch})
-#     for document in cursor:
-#         blocks = document.get('blocks')
-#         for block in blocks:
-#             if block.get('block_type') == 'openassessment':
-#                 fields = block.get('fields')
-#                 arr_submission_start = fields['submission_start'].split('+')
-#                 arr_submission_due = fields['submission_due'].split('+')
-#                 accessment_info = {'display_name': fields['display_name'], 'submission_start': arr_submission_start[0].replace('-', '').replace(':', '').replace('T', ''), 'submission_due': arr_submission_due[0].replace('-', '').replace(':', '').replace('T', '')}
-#     cursor.close()
-#     client.close()
-#
-#     return accessment_info
-#
-#
-# def create_temp_answer(course_id):
-#     reload(sys)
-#     sys.setdefaultencoding('utf-8')
-#     # TODO : ip
-#     # con = mdb.connect('192.168.1.112', 'root', '', 'edxapp');
-#     con = mdb.connect('localhost', 'root', '', 'edxapp');
-#     # query = "select uuid, raw_answer from submissions_submission where uuid in (select submission_uuid from assessment_peerworkflow where item_id not like '%DEMO%' and course_id = 'course-v1:EwhaK+EW10164K+2015-01')";
-#     query = "delete from tb_tmp_answer where course_id = '"+course_id+"'"
-#     cur = con.cursor()
-#     cur.execute(query)
-#
-#     query1 = "select uuid, raw_answer from submissions_submission "
-#     arr_course_id = course_id.split('+')
-#     query3 = "delete from vw_copykiller where class_id='"+arr_course_id[1]+"'"
-#     with con:
-#         cur.execute("set names utf8")
-#         cur.execute(query1)
-#         for (uuid, raw_answer) in cur:
-#             answer = raw_answer.replace('{"parts": [{"text": "', '').replace('"}]}','')
-#             answer = answer.decode('unicode_escape')
-#             answer = answer.replace("\'", "\\\'")
-#             answer = answer.encode('utf-8')
-#             answer = answer.decode('utf-8')
-#             query2 = "insert into tb_tmp_answer (course_id, uuid, raw_answer) "
-#             query2 += "select '"+course_id+"', '"+uuid+"', '"+answer+"' "
-#             query2 = str(query2)
-#             cur.execute(query2)
-#         cur.execute(query3)
-#     cur.close()
-#     con.close()
-#
-#
-# def copykiller(request, course_id):
-#     reload(sys)
-#     sys.setdefaultencoding('utf-8')
-#     course = return_course(course_id)
-#     assessment_info = get_assessment_info(course)
-#     create_temp_answer(course_id)
-#     # TODO : ip
-#     # con = mdb.connect('192.168.1.112', 'root', '', 'edxapp');
-#     con = mdb.connect('localhost', 'root', '', 'edxapp');
-#     query = "insert into vw_copykiller"
-#     query += "( uri, year_id, year_name, term_id, term_name, class_id, class_name, report_id, report_name,"
-#     query += "student_id, student_name, student_number, start_date, end_date, submit_date, title, content )"
-#     query += "select "
-#     query += "student_id, "
-#     query += "year(curdate()) year_id, concat(year(curdate()), '년') year_name, "
-#     query += "'" + str(course.id.run) + "' term_id, '" + str(course.id.run) + "' term_name, "
-#     query += "'" + str(course.id.course) + "' class_id, '"+ str(course.display_name) +"' class_name, "
-#     query += "item_id report_id, '"+str(assessment_info['display_name'])+"' report_name, "
-#     query += "(select user.username from auth_user user where user.id=(select anony.user_id from student_anonymoususerid anony where anony.anonymous_user_id=student_id)) student_id, "
-#     query += "(select profile.name from auth_userprofile profile where profile.id=(select anony.user_id from student_anonymoususerid anony where anony.anonymous_user_id=student_id)) student_name, "
-#     query += "(select user.id from auth_user user where user.id=(select anony.user_id from student_anonymoususerid anony where anony.anonymous_user_id=student_id)) student_number, "
-#     query += "'"+str(assessment_info['submission_start'])+"' start_date, "
-#     query += "'"+str(assessment_info['submission_due'])+"' end_date, "
-#     query += "completed_at submit_date, "
-#     query += "'"+str(assessment_info['display_name'])+"' title, "
-#     query += "(select answer.raw_answer from tb_tmp_answer answer where answer.uuid=submission_uuid) content "
-#     query += "from "
-#     query += "assessment_peerworkflow "
-#     query += "where "
-#     #query += "completed_at is not null and item_id not like '%DEMOk%';"
-#     query += "completed_at is not null;"
-#     query1 = "delete from tb_tmp_answer"
-#
-#     with con:
-#         cur = con.cursor()
-#         cur.execute("set names utf8")
-#         cur.execute(query)
-#         cur.execute(query1)
-#     cur.close()
-#     con.close()
-#
-#     response_data = {}
-#     response_data['result'] = 'success'
-#     return HttpResponse(simplejson.dumps(response_data), mimetype='application/javascript')
-#
-#
-#
-# def copykiller_csv(request, course_id):
-#     dict = get_copykiller_result(request, course_id)
-#
-#     response = HttpResponse(content_type='text/csv')
-#     response['Content-Disposition'] = 'attachment; filename="'+course_id+'.csv"'
-#
-#     writer = csv.writer(response)
-#     writer.writerow(['student id', 'assessment no', 'internet link', 'c lass', 'internet', 'report', 'term', 'total', 'year'])
-#     for value in dict.itervalues():
-#         writer.writerow(value)
-#
-#     return response
-#
-#
-# def get_copykiller_result(request, course_id):
-#     # TODO : ip
-#     # con = mdb.connect('192.168.1.112', 'root', '', 'edxapp');
-#     con = mdb.connect('localhost', 'root', '', 'edxapp');
-#     cur = con.cursor()
-#
-#     query = "select "
-#     query += "v.student_id, "
-#     query += "v.report_id assessment_no, "
-#     query += "'http://192.168.1.115/ckplus/copykiller.jsp?uri=ffcc76273516465fdde9181b3bb8f25c&property=100&lang=ko' internet_link, "
-#     query += "(select r.total_copy_ratio from tb_copykiller_copyratio r where r.uri=v.uri and r.check_type='class') class, "
-#     query += "(select r.total_copy_ratio from tb_copykiller_copyratio r where r.uri=v.uri and r.check_type='internet') internet, "
-#     query += "(select r.total_copy_ratio from tb_copykiller_copyratio r where r.uri=v.uri and r.check_type='report') report, "
-#     query += "(select r.total_copy_ratio from tb_copykiller_copyratio r where r.uri=v.uri and r.check_type='term') term, "
-#     query += "(select r.total_copy_ratio from tb_copykiller_copyratio r where r.uri=v.uri and r.check_type='total') total, "
-#     query += "(select r.total_copy_ratio from tb_copykiller_copyratio r where r.uri=v.uri and r.check_type='year') year "
-#     query += "from "
-#     query += "vw_copykiller v "
-#     query += "where "
-#     query += "v.uri in (select uri from tb_copykiller_copyratio) "
-#
-#     cur.execute(query)
-#     rows = cur.fetchall()
-#     dict = {}
-#     # tmp = list()
-#     for row in rows:
-#         # tmp = list(row[0:])
-#         # dict[str(row[0])] = tmp
-#         dict[str(row[0])] = list(row[0:])
-#     return dict
+def get_assessment_info(course):
+    # TODO : ip
+    # client = MongoClient('mongodb://192.168.1.112:27010/')
+    client = MongoClient()
+    db = client.edxapp
+    cursor = db.modulestore.active_versions.find({'search_targets.wiki_slug':course.wiki_slug})
+    for document in cursor:
+        published_branch = document.get('versions').get('published-branch')
+    cursor.close()
+
+    cursor = db.modulestore.structures.find({'_id':published_branch})
+    for document in cursor:
+        blocks = document.get('blocks')
+        for block in blocks:
+            if block.get('block_type') == 'openassessment':
+                fields = block.get('fields')
+                arr_submission_start = fields['submission_start'].split('+')
+                arr_submission_due = fields['submission_due'].split('+')
+                accessment_info = {'display_name': fields['display_name'], 'submission_start': arr_submission_start[0].replace('-', '').replace(':', '').replace('T', ''), 'submission_due': arr_submission_due[0].replace('-', '').replace(':', '').replace('T', '')}
+    cursor.close()
+    client.close()
+
+    return accessment_info
+
+
+def create_temp_answer(course_id):
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
+    # TODO : ip
+    # con = mdb.connect('192.168.1.112', 'root', '', 'edxapp');
+    con = mdb.connect('localhost', 'root', '', 'edxapp');
+    # query = "select uuid, raw_answer from submissions_submission where uuid in (select submission_uuid from assessment_peerworkflow where item_id not like '%DEMO%' and course_id = 'course-v1:EwhaK+EW10164K+2015-01')";
+    query = "delete from tb_tmp_answer where course_id = '"+course_id+"'"
+    cur = con.cursor()
+    cur.execute(query)
+
+    query1 = "select uuid, raw_answer from submissions_submission "
+    arr_course_id = course_id.split('+')
+    query3 = "delete from vw_copykiller where class_id='"+arr_course_id[1]+"'"
+    with con:
+        cur.execute("set names utf8")
+        cur.execute(query1)
+        for (uuid, raw_answer) in cur:
+            answer = raw_answer.replace('{"parts": [{"text": "', '').replace('"}]}','')
+            answer = answer.decode('unicode_escape')
+            answer = answer.replace("\'", "\\\'")
+            answer = answer.encode('utf-8')
+            answer = answer.decode('utf-8')
+            query2 = "insert into tb_tmp_answer (course_id, uuid, raw_answer) "
+            query2 += "select '"+course_id+"', '"+uuid+"', '"+answer+"' "
+            query2 = str(query2)
+            cur.execute(query2)
+        cur.execute(query3)
+    cur.close()
+    con.close()
+
+
+def copykiller(request, course_id):
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
+    course = return_course(course_id)
+    assessment_info = get_assessment_info(course)
+    create_temp_answer(course_id)
+    # TODO : ip
+    # con = mdb.connect('192.168.1.112', 'root', '', 'edxapp');
+    con = mdb.connect('localhost', 'root', '', 'edxapp');
+    query = "insert into vw_copykiller"
+    query += "( uri, year_id, year_name, term_id, term_name, class_id, class_name, report_id, report_name,"
+    query += "student_id, student_name, student_number, start_date, end_date, submit_date, title, content )"
+    query += "select "
+    query += "student_id, "
+    query += "year(curdate()) year_id, concat(year(curdate()), '년') year_name, "
+    query += "'" + str(course.id.run) + "' term_id, '" + str(course.id.run) + "' term_name, "
+    query += "'" + str(course.id.course) + "' class_id, '"+ str(course.display_name) +"' class_name, "
+    query += "item_id report_id, '"+str(assessment_info['display_name'])+"' report_name, "
+    query += "(select user.username from auth_user user where user.id=(select anony.user_id from student_anonymoususerid anony where anony.anonymous_user_id=student_id)) student_id, "
+    query += "(select profile.name from auth_userprofile profile where profile.id=(select anony.user_id from student_anonymoususerid anony where anony.anonymous_user_id=student_id)) student_name, "
+    query += "(select user.id from auth_user user where user.id=(select anony.user_id from student_anonymoususerid anony where anony.anonymous_user_id=student_id)) student_number, "
+    query += "'"+str(assessment_info['submission_start'])+"' start_date, "
+    query += "'"+str(assessment_info['submission_due'])+"' end_date, "
+    query += "completed_at submit_date, "
+    query += "'"+str(assessment_info['display_name'])+"' title, "
+    query += "(select answer.raw_answer from tb_tmp_answer answer where answer.uuid=submission_uuid) content "
+    query += "from "
+    query += "assessment_peerworkflow "
+    query += "where "
+    #query += "completed_at is not null and item_id not like '%DEMOk%';"
+    query += "completed_at is not null;"
+    query1 = "delete from tb_tmp_answer"
+
+    with con:
+        cur = con.cursor()
+        cur.execute("set names utf8")
+        cur.execute(query)
+        cur.execute(query1)
+    cur.close()
+    con.close()
+
+    response_data = {}
+    response_data['result'] = 'success'
+    return HttpResponse(simplejson.dumps(response_data), mimetype='application/javascript')
+
+
+
+def copykiller_csv(request, course_id):
+    dict = get_copykiller_result(request, course_id)
+
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="'+course_id+'.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(['student id', 'assessment no', 'internet link', 'c lass', 'internet', 'report', 'term', 'total', 'year'])
+    for value in dict.itervalues():
+        writer.writerow(value)
+
+    return response
+
+
+def get_copykiller_result(request, course_id):
+    # TODO : ip
+    # con = mdb.connect('192.168.1.112', 'root', '', 'edxapp');
+    con = mdb.connect('localhost', 'root', '', 'edxapp');
+    cur = con.cursor()
+
+    query = "select "
+    query += "v.student_id, "
+    query += "v.report_id assessment_no, "
+    query += "'http://192.168.1.115/ckplus/copykiller.jsp?uri=ffcc76273516465fdde9181b3bb8f25c&property=100&lang=ko' internet_link, "
+    query += "(select r.total_copy_ratio from tb_copykiller_copyratio r where r.uri=v.uri and r.check_type='class') class, "
+    query += "(select r.total_copy_ratio from tb_copykiller_copyratio r where r.uri=v.uri and r.check_type='internet') internet, "
+    query += "(select r.total_copy_ratio from tb_copykiller_copyratio r where r.uri=v.uri and r.check_type='report') report, "
+    query += "(select r.total_copy_ratio from tb_copykiller_copyratio r where r.uri=v.uri and r.check_type='term') term, "
+    query += "(select r.total_copy_ratio from tb_copykiller_copyratio r where r.uri=v.uri and r.check_type='total') total, "
+    query += "(select r.total_copy_ratio from tb_copykiller_copyratio r where r.uri=v.uri and r.check_type='year') year "
+    query += "from "
+    query += "vw_copykiller v "
+    query += "where "
+    query += "v.uri in (select uri from tb_copykiller_copyratio) "
+
+    cur.execute(query)
+    rows = cur.fetchall()
+    dict = {}
+    # tmp = list()
+    for row in rows:
+        # tmp = list(row[0:])
+        # dict[str(row[0])] = tmp
+        dict[str(row[0])] = list(row[0:])
+    return dict
