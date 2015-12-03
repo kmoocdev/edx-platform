4661bd4a6cf9cff2b3a32da9749134fdd194db7c
seongho <zudain@naver.com>
2015-12-03 11:04:38 +0000

    운영 db ip로 변경

--- lms/djangoapps/instructor/views/instructor_dashboard.py
+++ lms/djangoapps/instructor/views/instructor_dashboard.py
@@ -168,8 +168,8 @@ def instructor_dashboard_2(request, course_id):
 
 
 def check_assessment(active_versions_key):
-    # TODO : dev db ip
-    client = MongoClient('203.235.44.154', 27017)
+    # TODO : db ip
+    client = MongoClient('192.168.1.113', 27017)
     db = client.edxapp
 
     cursor = db.modulestore.active_versions.find({'search_targets.wiki_slug':active_versions_key})
@@ -611,7 +611,7 @@ def return_course(course_id):
 
 def get_assessment_info(course):
     # TODO : dev db ip
-    client = MongoClient('203.235.44.154', 27017)
+    client = MongoClient('192.168.1.113', 27017)
     db = client.edxapp
     cursor = db.modulestore.active_versions.find({'search_targets.wiki_slug':course.wiki_slug})
     for document in cursor:
@@ -637,7 +637,7 @@ def create_temp_answer(course_id):
     reload(sys)
     sys.setdefaultencoding('utf-8')
     # TODO : dev db ip
-    con = mdb.connect('203.235.44.154', 'root', '', 'edxapp');
+    con = mdb.connect('192.168.1.113', 'root', '', 'edxapp');
     # query = "select uuid, raw_answer from submissions_submission where uuid in (select submission_uuid from assessment_peerworkflow where item_id not like '%DEMO%' and course_id = 'course-v1:EwhaK+EW10164K+2015-01')";
     query = "delete from tb_tmp_answer where course_id = '"+course_id+"'"
     cur = con.cursor()
@@ -671,7 +671,7 @@ def copykiller(request, course_id):
     assessment_info = get_assessment_info(course)
     create_temp_answer(course_id)
     # TODO : dev db ip
-    con = mdb.connect('203.235.44.154', 'root', '', 'edxapp');
+    con = mdb.connect('192.168.1.113', 'root', '', 'edxapp');
     query = "insert into vw_copykiller"
     query += "( uri, year_id, year_name, term_id, term_name, class_id, class_name, report_id, report_name,"
     query += "student_id, student_name, student_number, start_date, end_date, submit_date, title, content )"
@@ -725,7 +725,7 @@ def copykiller_csv(request, course_id):
 
 def get_copykiller_result(request, course_id):
     # TODO : dev db ip
-    con = mdb.connect('203.235.44.154', 'root', '', 'edxapp');
+    con = mdb.connect('192.168.1.113', 'root', '', 'edxapp');
     cur = con.cursor()
 
     query = "select "
