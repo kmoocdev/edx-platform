585ab180d4a48277b9c7c708434496ef70ac6d9f
redukyo <redukyo@kotech.co.kr>
2016-01-26 05:50:52 +0000

    오픈api 페이지 개발중

--- lms/urls.py
+++ lms/urls.py
@@ -300,6 +300,10 @@ if settings.COURSEWARE_ENABLED:
         # url(r'^edit_circuit/(?P<circuit>[^/]*)$', 'circuit.views.edit_circuit'),
         # url(r'^save_circuit/(?P<circuit>[^/]*)$', 'circuit.views.save_circuit'),
         url(r'^courses/?$', 'branding.views.courses', name="courses"),
+        url(r'^openapi/?$', 'courseware.views.openapi', name="openapi"),
+        url(r'^openapi2/?$', 'courseware.views.openapi2', name="openapi2"),
+        url(r'^openapi3/?$', 'courseware.views.openapi3', name="openapi3"),
+        url(r'^openapi4/?$', 'courseware.views.openapi4', name="openapi4"),
         url(r'^schools/?$', 'courseware.views.schools', name="schools"),
 
         # url(r'^school/haewoondaex?$', 'courseware.views.haewoondaex', name="school"),
